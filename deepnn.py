84cf4e900de04ed786e0eb63f5af37b10b5263ed
kevintrankt <kevin.tran.kt@gmail.com>
2017-09-10 20:22:28 -0700

    Initialize Variables

--- deepnn.py
+++ deepnn.py
@@ -4,3 +4,17 @@ import matplotlib.pyplot as plt
 from testCases_v2 import *
 from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
+
+def initialize_parameters(n_x, n_h, n_y):
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
