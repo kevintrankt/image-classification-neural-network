ab86c4fc10c7cc3243625f890d114b13ef91457b
kevintrankt <kevin.tran.kt@gmail.com>
2017-09-10 20:59:04 -0700

    Implement sigmoid and relu activation functions

--- deepnn.py
+++ deepnn.py
@@ -40,3 +40,16 @@ def linear_forward(A, W, b):
     return Z, cache
 
 
+def linear_activation_forward(A_prev, W, b, activation):
+    Z, linear_cache = linear_forward(A_prev, W, b)
+
+    if activation == "sigmoid":
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == "relu":
+        A, activation_cache = relu(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
