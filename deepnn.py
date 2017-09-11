5617860e79569af7ae6250bfe57198bafd358bd4
kevintrankt <kevin.tran.kt@gmail.com>
2017-09-10 20:38:47 -0700

    Initialize Variables for Deep Neural Network

--- deepnn.py
+++ deepnn.py
@@ -1,10 +1,11 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-from testCases_v2 import *
-from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+# from testCases_v2 import *
+# from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
 
+# Initialize parameters for a 2 layer neural network
 def initialize_parameters(n_x, n_h, n_y):
     W1 = np.random.randn(n_h, n_x) * 0.01
     b1 = np.zeros((n_h, 1))
@@ -15,6 +16,18 @@ def initialize_parameters(n_x, n_h, n_y):
                   "b1": b1,
                   "W2": W2,
                   "b2": b2}
+
+    return parameters
+
+
+# Initialize parameters for a deep neural network where layer_dims contains the number of entries per layer
+def initialize_parameters_deep(layer_dims):
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
 
     return parameters
 
